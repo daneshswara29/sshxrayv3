60cbbe247dc68b24a3fd1aaa112f987d3a915ea1
daneshswara29 <44220709+daneshswara29@users.noreply.github.com>
2024-04-23 11:43:53 +0700

    Add files via upload

--- FILE/MENU/menu-ssh.sh
+++ FILE/MENU/menu-ssh.sh
@@ -217,7 +217,7 @@ portsshws=`cat ~/log-install.txt | grep -w "SSH Websocket" | cut -d: -f2 | awk '
 if [ -f "/etc/systemd/system/sshws.service" ]; then
 clear
 else
-wget -q -O /usr/bin/proxy3.js "https://raw.githubusercontent.com/daneshswara29/sshxrayv3/main/FILE/SSH/proxy3.js"
+wget -q -O /usr/bin/proxy3.js "https://raw.githubusercontent.com/daneshswara29/sshxrayv3/main/proxy3.js"
 cat <<EOF > /etc/systemd/system/sshws.service
 [Unit]
 Description=WSenabler
